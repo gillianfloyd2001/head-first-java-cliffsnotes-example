0894226745b6f538b92dcfb71739622ac6885a1b
Gillian Floyd <gfloyd@basecampcodingacademy.org>
2020-01-06 08:48:58 -0600

    fixed the spelling in string[]
    then changed the class name back to a upper case p

--- chapter-1-dive-in-a-quick-dip/PrintOutName.java
+++ chapter-1-dive-in-a-quick-dip/PrintOutName.java
@@ -1,5 +1,5 @@
-public class printOutName {
-    public static void main(Srring[] args) {
+public class PrintOutName {
+    public static void main(String[] args) {
         System.out.println("My name is Gillian");
     }
 }
