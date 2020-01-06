0f06bb81daf2cf3cec7184e434617bba22c30d34
Gillian Floyd <gfloyd@basecampcodingacademy.org>
2020-01-06 09:29:48 -0600

    a simple start with java for Chapter 2
    created a class named Chapter2 and then created a main function
    
    inside the main function it has a boolean and an if statement.
    
    this test to see if the name is gillian and if it is it print "Hello, Gillian"

---
+++ chapter-2-A-Trip-to-Objectville/Chapter2.java
@@ -0,0 +1,9 @@
+public class Chapter2 {
+    public static void main(String[] args) {
+        boolean isNameGillian = true;
+
+        if (isNameGillian == true) {
+            System.out.println("Hello, Gillian");
+        }
+    }
+}
