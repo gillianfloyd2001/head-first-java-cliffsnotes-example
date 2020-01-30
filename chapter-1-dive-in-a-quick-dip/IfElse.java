a7d489ef32216d28aa61a0576943b6b3dab3ae72
Gillian Floyd <gfloyd@basecampcodingacademy.org>
2020-01-30 12:49:05 -0600

    added an application that has branching.

---
+++ chapter-1-dive-in-a-quick-dip/IfElse.java
@@ -0,0 +1,11 @@
+class IfElse {
+    public static void main(String[] args) {
+        int a = 17;
+
+        if (a < 15) {
+            System.out.println("a is smaller than 15");
+        } else {
+            System.out.println("a is larger than 15");
+        }
+    }
+}
